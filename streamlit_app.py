b2ab8f7c94377d78a643cf48fd91f657eb5142b6
JyothiBapure <jyothi.patil4@gmail.com>
2026-02-12 00:49:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from models.data_loader import load_and_preprocess_data
 from models.logistic_regression_model import run_logistic_regression
+from models.decision_tree_model import run_decision_tree
+
 
 
 st.set_page_config(page_title="ML Assignment 2", layout="wide")
@@ -19,9 +21,23 @@ st.sidebar.markdown(
 
 # Load data
 X_train, X_test, y_train, y_test = load_and_preprocess_data(uploaded_test_file)
-
+model_choice = st.selectbox(
+    "Choose Model",
+    (
+        "Logistic Regression",
+        "Decision Tree Classifier",
+        "K-Nearest Neighbor Classifier",
+        "Naive Bayes Classifier",
+        "Ensemble Model - Random Forest",
+        "Ensemble Model - XGBoost"
+    )
+)
 # Run Model
-results = run_logistic_regression(X_train, X_test, y_train, y_test)
+if model_choice == "Logistic Regression":
+    results = run_logistic_regression(X_train, X_test, y_train, y_test)
+
+elif model_choice == "Decision Tree":
+    results = run_decision_tree(X_train, X_test, y_train, y_test)
 
 # Display Metrics
 st.subheader("Logistic Regression Results")
