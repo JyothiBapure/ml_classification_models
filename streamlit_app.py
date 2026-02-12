e162c1b11c21ff044d0ad506ae6c337b850578f7
JyothiBapure <jyothi.patil4@gmail.com>
2026-02-12 11:26:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -96,21 +96,21 @@ st.subheader("Confusion Matrix")
 st.write(results["confusion_matrix"])
 
 # Classification Report
-#st.subheader("Classification Report")
-#st.text(results["classification_report"])
-# Display classification report
 st.subheader("Classification Report")
-filtered_report = {k: v for k, v in results.items() if isinstance(v, dict)}
-report_df = pd.DataFrame.from_dict(filtered_report, orient="index")
+st.text(results["classification_report"])
+# Display classification report
+#st.subheader("Classification Report")
+#filtered_report = {k: v for k, v in results.items() if isinstance(v, dict)}
+#report_df = pd.DataFrame.from_dict(filtered_report, orient="index")
 
 # Add accuracy row safely
-if "accuracy" in results:
-    report_df.loc["accuracy", "precision"] = results["accuracy"]
-    report_df.loc["accuracy", "recall"] = np.nan
-    report_df.loc["accuracy", "f1-score"] = np.nan
-    report_df.loc["accuracy", "support"] = np.nan
+#if "accuracy" in results:
+   # report_df.loc["accuracy", "precision"] = results["accuracy"]
+    #report_df.loc["accuracy", "recall"] = np.nan
+    #report_df.loc["accuracy", "f1-score"] = np.nan
+    #report_df.loc["accuracy", "support"] = np.nan
 
 # Ensure numeric types and round
-report_df = report_df[["precision", "recall", "f1-score", "support"]].astype(float).round(4)
+#report_df = report_df[["precision", "recall", "f1-score", "support"]].astype(float).round(4)
 
-st.dataframe(report_df, width="stretch")
+#st.dataframe(report_df, width="stretch")
